0ff2837b5c767b6a37133b771161cb8054246221
Sandiprghane <64368583+Sandiprghane@users.noreply.github.com>
2020-07-24 17:01:21 +0530

    Updates

---
+++ precache-manifest.fb8a8e6522cccbbe3d7aa0c9a3e74575.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fec180e2174755ee4265735915c71d08",
+    "url": "/softNetTasks/index.html"
+  },
+  {
+    "revision": "8264e7382213e922294c",
+    "url": "/softNetTasks/static/css/main.2d89d07c.chunk.css"
+  },
+  {
+    "revision": "c6ddca98d6da27a5a590",
+    "url": "/softNetTasks/static/js/2.33376190.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/softNetTasks/static/js/2.33376190.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8264e7382213e922294c",
+    "url": "/softNetTasks/static/js/main.1a052e07.chunk.js"
+  },
+  {
+    "revision": "3aaf736c1832a568aa2f",
+    "url": "/softNetTasks/static/js/runtime-main.c0826aec.js"
+  }
+]);
